8de4765960db951b0d55520b47b0eef26951a744
Zhenru Peng <zpeng42@uwo.ca>
2023-03-21 12:55:56 -0400

    add okta movement and color change after hover or click

---
+++ games/OktaRescue/Assets/Scripts/okta.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class okta : MonoBehaviour
+{
+    public float speed = 100f;
+
+    private Color startcolor;
+	private SpriteRenderer oktaSR;
+    private Vector3 targetPosition;
+    private Vector3 startPos;
+
+	private bool isDown = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        oktaSR = GetComponent<SpriteRenderer>();
+        targetPosition = new Vector3(8, -4, 0);
+        startPos = transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDown) {
+            startPos = Vector3.MoveTowards(startPos, targetPosition, speed * Time.deltaTime);
+            transform.position = startPos;
+        }
+        if (transform.position == targetPosition) {
+            oktaSR.color = Color.green;
+        }
+
+    }
+
+
+    public void MoveTo(Vector3 position)
+    {
+        targetPosition = position;
+    }
+
+    // change the object to yellow when mouse hover
+    void OnMouseEnter()
+    {
+        startcolor = oktaSR.color;
+        oktaSR.color = Color.yellow;
+    }
+
+    void OnMouseExit()
+    {
+        oktaSR.color = startcolor;
+    }
+
+    // maybe in easy level let okta gets flushed
+    // change the object to yellow when mouse click okta and automatically move to a left position
+
+	void OnMouseDown() {
+		isDown = true;
+        oktaSR.color = Color.yellow;
+	}
+
+
+        
+}
