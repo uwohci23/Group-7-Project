b720bd00cd6711cb11a316fdcbe3435b1d68dd4f
phantomswordfish09 <dylannasution@gmail.com>
2023-03-22 23:47:23 -0400

    coin does not work

--- js/coinBox.js
+++ js/coinBox.js
@@ -3,6 +3,42 @@
 class CoinBox extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {
+            coinSelected: false
+        };
+
+        this.handleCoinDragStart = this.handleCoinDragStart.bind(this);
+        this.handleCoinDragMove = this.handleCoinDragMove.bind(this);
+        this.handleCoinDragEnd = this.handleCoinDragEnd.bind(this);
+    }
+
+    handleCoinDragStart() {
+        console.log("start");
+        this.setState({ coinSelected: true });
+    }
+
+    handleCoinDragMove(e) {
+        if (this.state.coinSelected) {
+            const coin = document.querySelector('.coin');
+            console.log(e.pageX, e.pageY)
+            coin.style.left = e.pageX +'px'; 
+            coin.style.top = e.pageY +'px';
+        }
+    }
+
+    handleCoinDragEnd() {
+        console.log("end");
+        this.setState({ coinSelected: false });
+    }
+
+    componentDidUpdate(props, state) {
+        if (this.state.coinSelected && !state.coinSelected) {
+            document.addEventListener('mousemove', this.onMouseMove)
+            document.addEventListener('mouseup', this.onMouseUp)
+        } else if (!this.state.coinSelected && state.coinSelected) {
+            document.removeEventListener('mousemove', this.onMouseMove)
+            document.removeEventListener('mouseup', this.onMouseUp)
+        }
     }
 
     render() {
@@ -14,7 +50,7 @@ class CoinBox extends React.Component {
                     <div className="buttons-area">Buttons
                     
                     </div>
-                    
+
                     <div className="from-floor-area">Exchange coins from floor
                     
                     </div>
@@ -23,11 +59,14 @@ class CoinBox extends React.Component {
                     
                     </div>
 
-                    <div className="bank-account">Coins from bank
-                    
+                    <div className="bank-account" >Coins from bank
+                        
                     </div>
 
                     <div className="floor">Floor
+                        <div id ="coin" className="coin" onMouseDown={this.handleCoinDragStart} onMouseMove={this.handleCoinDragMove} onMouseUp={this.handleCoinDragEnd}>
+
+                        </div>
                     
                     </div>
                 </div>
