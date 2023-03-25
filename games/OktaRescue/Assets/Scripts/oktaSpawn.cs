b7e713ccac8926184c4175a1ef9446bf6c89b749
Zhenru Peng <zpeng42@uwo.ca>
2023-03-24 20:47:20 -0400

    okta spawns! although they overlap :(

--- games/OktaRescue/Assets/Scripts/oktaSpawn.cs
+++ games/OktaRescue/Assets/Scripts/oktaSpawn.cs
@@ -2,15 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+// OKTA OVERLAP EACH OTHER!!!
 public class oktaSpawn : MonoBehaviour
 {
     public GameObject okta;
-
+    private int maxOkta = 20;
+    private List<GameObject> oktaList;
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(okta, transform.position, transform.rotation);
+        oktaList = new List<GameObject>();
+        float height = Camera.main.orthographicSize;
+		float width = height * Camera.main.aspect;
+        height -= 1f;
+		width -= 1f;
 
+       	for (int i = 0; i < maxOkta; i++) {
+            Debug.Log(i);
+            Vector3 pos = new Vector3(Random.Range(-width + 1, width - 3), Random.Range(-height, height - 1), 0f);
+
+            Instantiate(okta, pos, transform.rotation);
+        }
     }
 
     // Update is called once per frame
