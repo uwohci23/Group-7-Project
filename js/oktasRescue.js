c8b793a2660dce07dbc268b59c01e1e0d3036efa
phantomswordfish09 <dylannasution@gmail.com>
2023-04-03 02:01:44 -0400

    Restart works for iframes now

--- js/oktasRescue.js
+++ js/oktasRescue.js
@@ -5,10 +5,17 @@ class OktasRescue extends React.Component {
         super(props)
     }
 
+    restart() {
+        // this is for testing
+        // it currently uses grazing iframe but it should use Okta iframe
+        document.getElementById("Oktas Rescue").src= "../games/GroupingAndGrazing/Build/index.html";
+    }
+
     render() {
         return (
             <div>
-                <PopupMenu name="Okta's Rescue" onClick={this.props.onClick} />
+                <PopupMenu name="Oktas Rescue" onClick={this.props.onClick} restart={this.restart} />
+                <iframe id="Oktas Rescue" src="../games/GroupingAndGrazing/Build/index.html" style={{ width: "100vw", height: "100vh" }}></iframe>
             </div>
         )
     }
