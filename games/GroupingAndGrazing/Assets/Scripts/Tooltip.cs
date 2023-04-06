723f66d6c2d1280ca0bcf18f823667d591300398
Ryan <ryantruong927@gmail.com>
2023-04-06 16:15:08 -0400

    Final build
    - added tutorial build
    - cows now highlight when moused over

---
+++ games/GroupingAndGrazing/Assets/Scripts/Tooltip.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tooltip : MonoBehaviour
+{
+    private void OnMouseEnter() {
+        
+    }
+
+    private void OnMouseExit() {
+        
+    }
+}
