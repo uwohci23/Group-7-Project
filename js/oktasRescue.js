9a17ed2e7c5aa79b7c17cf0ea4f1289e77e4f0b7
Ryan <ryantruong927@gmail.com>
2023-04-06 21:23:57 -0400

    Okta Build

--- js/oktasRescue.js
+++ js/oktasRescue.js
@@ -8,17 +8,17 @@ class OktasRescue extends React.Component {
     restart() {
         // this is for testing
         // it currently uses grazing iframe but it should use Okta iframe
-        document.getElementById("Oktas Rescue").src = "../games/OktasRescue/Build/index.html";
+        document.getElementById("Oktas Rescue").src = "../games/OktaRescue/Build/index.html";
     }
 
     render() {
         let framer
         switch (this.props.isTutorial) {
             case false:
-                framer = <iframe id="Oktas Rescue" src="../games/OktasRescue/Build/index.html" style={{ width: "100vw", height: "100vh" }}></iframe>
+                framer = <iframe id="Oktas Rescue" src="../games/OktaRescue/Build/index.html" style={{ width: "100vw", height: "100vh" }}></iframe>
                 break;
             case true:
-                framer = <iframe id="Oktas Rescue" src="../games/OktasRescue/TutorialBuild/index.html" style={{ width: "100vw", height: "100vh" }}></iframe>
+                framer = <iframe id="Oktas Rescue" src="../games/OktaRescue/TutorialBuild/index.html" style={{ width: "100vw", height: "100vh" }}></iframe>
                 break;
         }
         return (
